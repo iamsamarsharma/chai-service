d02dc97dc65290834c7a05a6aea912ec6d6e9f8e
samar@DESKTOP-GAVQ5IT <samarsharma@outlook.com>
2019-08-07 00:05:54 +0530

    Initial commit

---
+++ src/main/java/com/spring/mssg/chaiservice/ChaiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.mssg.chaiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaiServiceApplication.class, args);
+	}
+
+}
